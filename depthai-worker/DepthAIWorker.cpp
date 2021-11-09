5d77a55e09f06480a1afb46fedbd067251b1b618
Dan Weatherford <sabretooth@gmail.com>
2021-11-09 14:48:50 -0600

    DepthAI worker: thread-per-device

--- depthai-worker/DepthAIWorker.cpp
+++ depthai-worker/DepthAIWorker.cpp
@@ -9,15 +9,14 @@
 
 #include <time.h>
 #include <chrono>
+#include <thread>
+#include <boost/thread/barrier.hpp>
 
 static inline uint64_t currentTimeNs() {
   struct timespec ts;
   clock_gettime(CLOCK_MONOTONIC, &ts);
   return (ts.tv_sec * 1000000000ULL) + ts.tv_nsec;
 }
-
-
-static SHMSegment<DepthMapSHM>* shm;
 
 
 struct PerViewData {
@@ -28,60 +27,38 @@ struct PerViewData {
   unsigned int m_pipelineWidth, m_pipelineHeight;
 
 
+  std::thread processingThread;
+
   std::shared_ptr<dai::Device> device;
   std::shared_ptr<dai::DataInputQueue> leftQueue, rightQueue, configQueue;
   std::shared_ptr<dai::DataOutputQueue> dispQueue;
 };
 
-int main(int argc, char* argv[]) {
-  int ppid = getppid();
+static SHMSegment<DepthMapSHM>* shm;
+PerViewData perViewData[DepthMapSHM::maxViews];
+uint32_t frameSequenceNumber = 0;
+boost::barrier frameStartBarrier(DepthMapSHM::maxViews + 1);
+boost::barrier frameEndBarrier(DepthMapSHM::maxViews + 1);
+std::mutex deviceBootMutex;
 
-  shm = SHMSegment<DepthMapSHM>::openSegment("depth-worker");
-  if (!shm) {
-    printf("dgpu-worker: Unable to open SHM segment\n");
-    return -1;
-  }
-  PerViewData perViewData[DepthMapSHM::maxViews];
 
-  // Signal readyness
-  sem_post(&shm->segment()->m_workerReadySem);
-
-  uint32_t frameSequenceNumber = 0;
+void viewProcessingThread(size_t viewIdx) {
+  DepthMapSHM::ViewParams& vp = shm->segment()->m_viewParams[viewIdx];
+  PerViewData& viewData = perViewData[viewIdx];
 
   while (true) {
-    {
-      struct timespec ts;
-      ts.tv_sec = 1;
-      ts.tv_nsec = 0;
-      if (sem_timedwait(&shm->segment()->m_workAvailableSem, &ts) < 0) {
-        if (errno == ETIMEDOUT) {
-          if (kill(ppid, 0) != 0) {
-            printf("dgpu-worker: parent process %d has exited\n", ppid);
-            return 0;
-          }
-          continue;
-        } else {
-          perror("sem_timedwait");
-          return -1;
-        }
-      }
-    }
 
-    uint64_t startTime = currentTimeNs();
+    // Wait for frame start
+    frameStartBarrier.wait();
 
-    // Check and update configuration on active views
-    for (size_t viewIdx = 0; viewIdx < shm->segment()->m_activeViewCount; ++viewIdx) {
-      DepthMapSHM::ViewParams& vp = shm->segment()->m_viewParams[viewIdx];
-      PerViewData& viewData = perViewData[viewIdx];
-      if (viewData.m_lastSettingsGeneration == shm->segment()->m_settingsGeneration)
-        continue;
+    // Make sure view is active
+    if (viewIdx < shm->segment()->m_activeViewCount) {
 
       if (!viewData.device || (viewData.m_pipelineWidth != vp.width) || (viewData.m_pipelineHeight != vp.height)) {
         // Newly attached stream, or the pipeline dimensions have changed.
 
         // Detach any previously attached device
         viewData.device.reset();
-
 
         // Build pipeline components
         dai::Pipeline pipeline;
@@ -90,7 +67,7 @@ int main(int argc, char* argv[]) {
         viewData.m_pipelineHeight = vp.height;
         viewData.m_paddedWidth = (vp.width + 15) & (~(15UL)); // Pad width up to 16px boundary
 
-        printf("Creating pipeline for stream %zu: %u x %u (padded: %u x %u)\n", viewIdx, vp.width, vp.height, viewData.m_paddedWidth, vp.height);
+        printf("[%zu] Creating stream pipeline: %u x %u (padded: %u x %u)\n", viewIdx, vp.width, vp.height, viewData.m_paddedWidth, vp.height);
 
         auto inLeft = pipeline.create<dai::node::XLinkIn>();
         auto inRight = pipeline.create<dai::node::XLinkIn>();
@@ -124,8 +101,12 @@ int main(int argc, char* argv[]) {
         stereo->disparity.link(outDisparity->input);
 
         // Start the pipeline on the first available device
-        viewData.device = std::make_shared<dai::Device>(pipeline);
-        printf("Stream %zu processing started on device MXID=%s\n", viewIdx, viewData.device->getMxId().c_str());
+        {
+          // Synchronize access to the global device list, so two threads don't pick and try to initialize the same device
+          std::lock_guard<std::mutex> g(deviceBootMutex);
+          viewData.device = std::make_shared<dai::Device>(pipeline);
+        }
+        printf("[%zu] Processing started on device MXID=%s\n", viewIdx, viewData.device->getMxId().c_str());
 
         // Wire up the device queues
         viewData.leftQueue   =  viewData.device->getInputQueue("left",   /*maxSize=*/ 8, /*blocking=*/ false);
@@ -133,84 +114,140 @@ int main(int argc, char* argv[]) {
         viewData.configQueue =  viewData.device->getInputQueue("config", /*maxSize=*/ 4, /*blocking=*/ false);
         viewData.dispQueue   =  viewData.device->getOutputQueue("disp",  /*maxSize=*/ 8, /*blocking=*/ false);
 
+        // Force config update
+        viewData.m_lastSettingsGeneration = shm->segment()->m_settingsGeneration - 1;
       } // pipeline creation
 
-      // Update config
-      dai::StereoDepthConfig config;
-      config.setConfidenceThreshold(shm->segment()->m_confidenceThreshold);
-      config.setMedianFilter((dai::MedianFilter) shm->segment()->m_medianFilter);
-      config.setBilateralFilterSigma(shm->segment()->m_bilateralFilterSigma);
-      config.setLeftRightCheckThreshold(shm->segment()->m_leftRightCheckThreshold);
-      config.setLeftRightCheck(shm->segment()->m_enableLRCheck);
+      if (viewData.m_lastSettingsGeneration != shm->segment()->m_settingsGeneration) {
+        // Update config
+        dai::StereoDepthConfig config;
+        config.setConfidenceThreshold(shm->segment()->m_confidenceThreshold);
+        config.setMedianFilter((dai::MedianFilter) shm->segment()->m_medianFilter);
+        config.setBilateralFilterSigma(shm->segment()->m_bilateralFilterSigma);
+        config.setLeftRightCheckThreshold(shm->segment()->m_leftRightCheckThreshold);
+        config.setLeftRightCheck(shm->segment()->m_enableLRCheck);
 
-      viewData.configQueue->send(config);
+        viewData.configQueue->send(config);
 
-      viewData.m_lastSettingsGeneration = shm->segment()->m_settingsGeneration;
+        viewData.m_lastSettingsGeneration = shm->segment()->m_settingsGeneration;
+      }
+
+      // Submit input frames
+      {
+        auto submitTime = std::chrono::steady_clock::now();
+        dai::ImgFrame l, r;
+        {
+          l.getData().resize(viewData.m_paddedWidth * vp.height);
+          unsigned char* outBase = l.getData().data();
+          const char* inBase = shm->segment()->data() + vp.inputLeftOffset;
+          for (size_t row = 0; row < vp.height; ++row)
+            memcpy(outBase + (row * viewData.m_paddedWidth), inBase + (row * vp.inputPitchBytes), vp.width);
+        }
+        l.setWidth(viewData.m_paddedWidth);
+        l.setHeight(vp.height);
+        l.setType(dai::ImgFrame::Type::GRAY8);
+        l.setInstanceNum((unsigned int) dai::CameraBoardSocket::LEFT);
+        l.setSequenceNum(frameSequenceNumber);
+        l.setTimestamp(submitTime);
+
+        {
+          r.getData().resize(viewData.m_paddedWidth * vp.height);
+          unsigned char* outBase = r.getData().data();
+          const char* inBase = shm->segment()->data() + vp.inputRightOffset;
+          for (size_t row = 0; row < vp.height; ++row)
+            memcpy(outBase + (row * viewData.m_paddedWidth), inBase + (row * vp.inputPitchBytes), vp.width);
+        }
+
+        r.setWidth(viewData.m_paddedWidth);
+        r.setHeight(vp.height);
+        r.setType(dai::ImgFrame::Type::GRAY8);
+        r.setInstanceNum((unsigned int) dai::CameraBoardSocket::RIGHT);
+        r.setSequenceNum(frameSequenceNumber);
+        r.setTimestamp(submitTime);
+
+        viewData.leftQueue->send(l);
+        viewData.rightQueue->send(r);
+      }
+
+      // Download results
+      {
+        PerViewData& viewData = perViewData[viewIdx];
+        DepthMapSHM::ViewParams& vp = shm->segment()->m_viewParams[viewIdx];
+
+        // StereoBM uses CV_8UC1 disparity map, everything else uses CV_16SC1
+        size_t bytesPerPixel = 1; // TODO support 16bpp
+
+        auto frame = viewData.dispQueue->get<dai::ImgFrame>();
+        assert(frame->getType() == dai::ImgFrame::Type::RAW8 || frame->getType() == dai::ImgFrame::Type::GRAY8);
+        assert(frame->getData().size() == frame->getWidth() * frame->getHeight());
+        assert(frame->getWidth() == viewData.m_paddedWidth);
+        assert(frame->getHeight() == vp.height);
+
+        for (size_t row = 0; row < vp.height; ++row) {
+          memcpy((shm->segment()->data() + vp.outputOffset) + (vp.outputPitchBytes * row), frame->getData().data() + (viewData.m_paddedWidth * bytesPerPixel * row), (vp.width * bytesPerPixel));
+        }
+      }
+    } else {
+      // ensure device is released
+      if (viewData.device) {
+        viewData.device.reset();
+        viewData.leftQueue.reset();
+        viewData.rightQueue.reset();
+        viewData.configQueue.reset();
+        viewData.dispQueue.reset();
+      }
     }
 
-    auto submitTime = std::chrono::steady_clock::now();
+    // Signal completion
+    frameEndBarrier.wait();
 
-    // Submit input frames
-    for (size_t viewIdx = 0; viewIdx < shm->segment()->m_activeViewCount; ++viewIdx) {
-      DepthMapSHM::ViewParams& vp = shm->segment()->m_viewParams[viewIdx];
-      PerViewData& viewData = perViewData[viewIdx];
-
-      //cv::Mat leftMat(vp.height, vp.width, CV_8UC1, shm->segment()->data() + vp.inputLeftOffset, vp.inputPitchBytes);
-      //cv::Mat rightMat(vp.height, vp.width, CV_8UC1, shm->segment()->data() + vp.inputRightOffset, vp.inputPitchBytes);
-
-      dai::ImgFrame l, r;
-      {
-        l.getData().resize(viewData.m_paddedWidth * vp.height);
-        unsigned char* outBase = l.getData().data();
-        const char* inBase = shm->segment()->data() + vp.inputLeftOffset;
-        for (size_t row = 0; row < vp.height; ++row)
-          memcpy(outBase + (row * viewData.m_paddedWidth), inBase + (row * vp.inputPitchBytes), vp.width);
-      }
-      l.setWidth(viewData.m_paddedWidth);
-      l.setHeight(vp.height);
-      l.setType(dai::ImgFrame::Type::GRAY8);
-      l.setInstanceNum((unsigned int) dai::CameraBoardSocket::LEFT);
-      l.setSequenceNum(frameSequenceNumber);
-      l.setTimestamp(submitTime);
-
-      {
-        r.getData().resize(viewData.m_paddedWidth * vp.height);
-        unsigned char* outBase = r.getData().data();
-        const char* inBase = shm->segment()->data() + vp.inputRightOffset;
-        for (size_t row = 0; row < vp.height; ++row)
-          memcpy(outBase + (row * viewData.m_paddedWidth), inBase + (row * vp.inputPitchBytes), vp.width);
-      }
-
-      r.setWidth(viewData.m_paddedWidth);
-      r.setHeight(vp.height);
-      r.setType(dai::ImgFrame::Type::GRAY8);
-      r.setInstanceNum((unsigned int) dai::CameraBoardSocket::RIGHT);
-      r.setSequenceNum(frameSequenceNumber);
-      r.setTimestamp(submitTime);
-
-      viewData.leftQueue->send(l);
-      viewData.rightQueue->send(r);
-    } // View loop
+  } // frame loop
+}
 
 
-    // Download all results
-    for (size_t viewIdx = 0; viewIdx < shm->segment()->m_activeViewCount; ++viewIdx) {
-      PerViewData& viewData = perViewData[viewIdx];
-      DepthMapSHM::ViewParams& vp = shm->segment()->m_viewParams[viewIdx];
+int main(int argc, char* argv[]) {
+  int ppid = getppid();
 
-      // StereoBM uses CV_8UC1 disparity map, everything else uses CV_16SC1
-      size_t bytesPerPixel = 1; // TODO support 16bpp
+  shm = SHMSegment<DepthMapSHM>::openSegment("depth-worker");
+  if (!shm) {
+    printf("dgpu-worker: Unable to open SHM segment\n");
+    return -1;
+  }
 
-      auto frame = viewData.dispQueue->get<dai::ImgFrame>();
-      assert(frame->getType() == dai::ImgFrame::Type::RAW8 || frame->getType() == dai::ImgFrame::Type::GRAY8);
-      assert(frame->getData().size() == frame->getWidth() * frame->getHeight());
-      assert(frame->getWidth() == viewData.m_paddedWidth);
-      assert(frame->getHeight() == vp.height);
+  // Launch processing threads
+  for (size_t viewIdx = 0; viewIdx < DepthMapSHM::maxViews; ++viewIdx) {
+    perViewData[viewIdx].processingThread = std::thread(viewProcessingThread, viewIdx);
+  }
 
-      for (size_t row = 0; row < vp.height; ++row) {
-        memcpy((shm->segment()->data() + vp.outputOffset) + (vp.outputPitchBytes * row), frame->getData().data() + (viewData.m_paddedWidth * bytesPerPixel * row), (vp.width * bytesPerPixel));
+  // Signal readyness
+  sem_post(&shm->segment()->m_workerReadySem);
+
+  while (true) {
+    {
+      struct timespec ts;
+      ts.tv_sec = 1;
+      ts.tv_nsec = 0;
+      if (sem_timedwait(&shm->segment()->m_workAvailableSem, &ts) < 0) {
+        if (errno == ETIMEDOUT) {
+          if (kill(ppid, 0) != 0) {
+            printf("dgpu-worker: parent process %d has exited\n", ppid);
+            return 0;
+          }
+          continue;
+        } else {
+          perror("sem_timedwait");
+          return -1;
+        }
       }
     }
+
+    uint64_t startTime = currentTimeNs();
+
+    // Start processing threads
+    frameStartBarrier.wait();
+
+    // Wait for processing threads to finish
+    frameEndBarrier.wait();
 
     for (size_t viewIdx = 0; viewIdx < shm->segment()->m_activeViewCount; ++viewIdx) {
       //PerViewData& viewData = perViewData[viewIdx];
