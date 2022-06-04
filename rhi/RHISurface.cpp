0c7b2de7a5a9d304bc8394542f8de2ce82a4f529
Dan Weatherford <sabretooth@gmail.com>
2022-06-04 17:52:18 -0500

    Add missing enums to rhiSurfaceFormatSize

--- rhi/RHISurface.cpp
+++ rhi/RHISurface.cpp
@@ -10,10 +10,17 @@ size_t rhiSurfaceFormatSize(RHISurfaceFormat format) {
       assert(false && "rhiSurfaceFormatSize: unhandled enum");
 
     case kSurfaceFormat_R8:
+    case kSurfaceFormat_R8i:
+    case kSurfaceFormat_R8ui:
     case kSurfaceFormat_Stencil8:
       return 1;
 
+    case kSurfaceFormat_R16:
     case kSurfaceFormat_R16f:
+    case kSurfaceFormat_R16i:
+    case kSurfaceFormat_R16ui:
+    case kSurfaceFormat_RG8i:
+    case kSurfaceFormat_RG8ui:
     case kSurfaceFormat_Depth16:
       return 2;
 
@@ -22,6 +29,12 @@ size_t rhiSurfaceFormatSize(RHISurfaceFormat format) {
     case kSurfaceFormat_RGB10_A2:
     case kSurfaceFormat_RG16:
     case kSurfaceFormat_R32f:
+    case kSurfaceFormat_R32i:
+    case kSurfaceFormat_R32ui:
+    case kSurfaceFormat_RG16i:
+    case kSurfaceFormat_RG16ui:
+    case kSurfaceFormat_RGBA8i:
+    case kSurfaceFormat_RGBA8ui:
     case kSurfaceFormat_Depth32f:
       return 4;
 
@@ -32,7 +45,15 @@ size_t rhiSurfaceFormatSize(RHISurfaceFormat format) {
     case kSurfaceFormat_RGBA16f:
     case kSurfaceFormat_RGBA16s:
     case kSurfaceFormat_RG32f:
+    case kSurfaceFormat_RG32i:
+    case kSurfaceFormat_RG32ui:
+    case kSurfaceFormat_RGBA16i:
+    case kSurfaceFormat_RGBA16ui:
       return 8;
+
+    case kSurfaceFormat_RGBA32i:
+    case kSurfaceFormat_RGBA32ui:
+      return 16;
   };
 }
 
