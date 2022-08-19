787d44d7bd736d221f89567f595c47a1e9ab07f8
Dan Weatherford <sabretooth@gmail.com>
2022-08-19 15:15:50 -0500

    Missed shaders for stereo-geometry tool

---
+++ shaders/frustumVisualize.vtx.glsl
@@ -0,0 +1,15 @@
+#version 320 es
+in vec3 position;
+out vec4 fragColor;
+
+layout(std140) uniform FrustumVisualizeUniformBlock {
+  mat4 viewProjection;
+  mat4 frustumViewProjectionInverse;
+  vec4 color;
+};
+
+void main() {
+  gl_Position = viewProjection * (frustumViewProjectionInverse * vec4(position, 1.0));
+  fragColor = color;
+}
+
