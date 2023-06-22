9d4d010156eed084e00ee5c98f0df06a0599506c
Dan Weatherford <sabretooth@gmail.com>
2023-06-22 17:18:47 -0500

    gitignore update

---
+++ tags.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+ctags -R \
+  common \
+  depthai-worker \
+  dgpu-fans \
+  dgpu-worker \
+  eyectl \
+  hmdcam \
+  imgui/*.h \
+  imgui/*.cpp \
+  implot \
+  live555/include \
+  pductl \
+  rdma \
+  rdma-client \
+  rhi \
+  stb \
+  tegra_mmapi \
+  vulkan \
+  wayland \
+  XLink/include \
+  XLink/src \
+
