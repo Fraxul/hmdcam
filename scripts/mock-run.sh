fbc3c618bbc041c62086ccb24d60093db9838c30
Dan Weatherford <sabretooth@gmail.com>
2024-07-12 15:45:51 -0500

    Reorganize some utility scripts

---
+++ scripts/mock-run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+build/bin/hmdcam --debug-mock-cameras --depth-backend none --disable-pdu --render-backend vulkan
---
+++ scripts/worker-benchmark.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+build/bin/worker-benchmark --depth-backend depthai --res 480 270 --views 2
+
