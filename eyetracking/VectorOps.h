46c8d30e4f679b45846d8efa15c0ffc586fc31ae
Dan Weatherford <sabretooth@gmail.com>
2026-01-23 16:58:25 -0600

    Move common vector code out of EyeTrackingService

---
+++ eyetracking/VectorOps.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdint.h>
+#include <unistd.h>
+#include <arm_neon.h>
+
+void convertUnorm8ToSnormFp16(const uint8_t* inU8, void* outFP16, const size_t elementCount);
+void convertUnorm8ToDLAInt8(const uint8_t* inU8, void* outDLAInt8, size_t elementCount);
+void fp16ThresholdToU8Mask(const _Float16* inFP16, _Float16 thresholdValue, uint8_t* outU8, size_t elementCount);
+_Float16 fp16VectorMax(const _Float16* inFP16, size_t elementCount);
+
